3f5c44f034bcf6b90828ef58a8e21d182f5ecdc7
dushriane <arianedushime@gmail.com>
2025-08-10 20:17:39 +0200

    added frontend deployed link

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500','http://localhost:5500','http://127.0.0.1:3000','http://localhost:3000'],
+  origin: ['http://127.0.0.1:5500','http://localhost:5500','http://127.0.0.1:3000','http://localhost:3000', 'https://arnprofessionalporfolio.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 
